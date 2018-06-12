9bd357ae5dca474248778b0db669d5db9e72c02a
Melvin_Homefront <melvinkusters0@gmail.com>
2018-06-12 07:19:56 +0200

    GetCoursesForTeacher procedure implementation completed.

---
+++ SQLOefeningForm/Procedure_GetCoursesForTeacher.sql
@@ -0,0 +1,26 @@
+﻿USE SQLOefeningLocalDb;  
+GO  
+CREATE PROCEDURE GetCoursesForTeacher
+    @Name varchar(50),   
+    @Surname varchar(50)   
+AS   
+    SELECT Title
+    FROM COURSE  
+    WHERE Title IN (
+		SELECT COURSE 
+		FROM COURSESUBJECTS
+		WHERE SUBJECT IN (
+			SELECT Title
+			FROM SUBJECT
+			WHERE Title IN (
+				SELECT SUBJECT
+				FROM SUBJECTTEACHERS
+				WHERE TEACHER IN (
+					SELECT Id
+					FROM TEACHER
+					WHERE Name = @Name AND Surname = @Surname
+					))));
+GO  
+
+EXEC GetCoursesForTeacher N'Bart',N'Schreurs';
+GO
